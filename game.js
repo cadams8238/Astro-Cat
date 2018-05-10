7e0f1f5e64c44b6a1c306c9886e43946c77acd4c
Courtney Adams <cadams8238@gmail.com>
2018-05-10 15:56:17 -0700

    cleaned up code style, included comments and general organization

--- game.js
+++ game.js
@@ -15,11 +15,8 @@ unnecessarily.
 
 function setup() {
   
-  createCanvas(600, 600);
-  background(0);
-  createBackground();
-
-  cat = new Cat(100,100,50);
+ 	createBackground();
+	cat = new Cat(100,100,50);
 
 
 }
@@ -38,29 +35,81 @@ function draw() {
 }
 
 
+
+/*
+---------------------------------------------------------
+keyPressed()
+---------------------------------------------------------
+*/
+
+function keyPressed() {
+
+}
+
+
 /*
 ---------------------------------------------------------
 createBackground() draws out the background for the game.
 This includes the outline for the gameboard, the lighter
-black box at the bottom of gameboard, and the stars.
+black box at the bottom of gameboard, and the stars
+all packed neatly in their own functions
 ---------------------------------------------------------
 */
 
 function createBackground() {
 
-	//creates outline around gameboard
+	 	createCanvas(600, 600);
+	 	background(0);
+	 	drawOutline();
+	 	drawMilkyWay();
+	 	drawStars();
+ 
+  
+
+}
+
+/*
+---------------------------------------------------------
+drawOutline() creates outline around gameboard
+---------------------------------------------------------
+*/
+
+function drawOutline() {
+
 	stroke('#EBEBD3');
   strokeWeight(4);
   noFill();	//noFill needed so that the black background will show through
   rect(0,0,600,600);
 
-  //creates "Milky way", lighter black box at bottom of gameboard
-  fill('#1F2223');
+}
+
+
+/*
+---------------------------------------------------------
+drawMilkyWay() creates "Milky way", lighter black box at 
+bottom of gameboard
+---------------------------------------------------------
+*/
+
+function drawMilkyWay() {
+
+	fill('#1F2223');
   noStroke();
   rect(2,298,596,300); //values take outer stroke into concideration and don't cover it up, stroke = 2 px (approx)
-  
-  //creates stars
-  for (let i = 0; i < 150; i++) {
+
+}
+
+
+
+/*
+---------------------------------------------------------
+drawStars() creates the stars in the game
+---------------------------------------------------------
+*/
+
+function drawStars() {
+
+	for (let i = 0; i < 150; i++) {
 
   	let x = random(1,width),
   			y = random(1,height),
@@ -70,39 +119,9 @@ function createBackground() {
   	noStroke();
   	ellipse(x,y,diameter);
   }
-
 }
 
 
-// function Cat(x,y,widthHeight) {
-
-
-// 	//assuming the cat is a square, keeps track of all 4 corners
-// 	//t = top, b = bottom
-
-// 	this.tLeft = x;
-// 	this.tRight = x + widthHeight;
-// 	this.bLeft = y;
-// 	this.bRight = y + widthHeight;
-
-
-// 	this.intersects = function(obstacle) {
-// 		return !(this.left > obstacle.right || this.right < obstacle.left ||
-// 						 this.right > obstacle.left || this.left < obstacle.right);
-// 	}
-
-// 	this.show = function() {
-// 		fill('white');
-// 		rect(100, 100, 50);
-// 	}
-
-
-// 	update = function() {
-
-// 	}
-
-
-// }
 
 class object {
 
@@ -131,7 +150,7 @@ class Cat extends object {
 	}
 
 
-	update() {
+	move() {
 
 	}
 }
